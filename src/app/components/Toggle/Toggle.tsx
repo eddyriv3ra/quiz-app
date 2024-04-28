6a84da963d09be4d8e5cf1c6a89431927d5c48b5
Eddy Rivera Echegaray <eddyriveraechegaray@Eddys-MBP.homenet.telecomitalia.it>
2024-04-28 21:19:34 +0200

    dark mode toggle button

--- src/app/components/Toggle/Toggle.tsx
+++ src/app/components/Toggle/Toggle.tsx
@@ -1,4 +1,7 @@
 import { useTheme } from "next-themes";
+import * as Switch from "@radix-ui/react-switch";
+import styles from "./Toggle.module.scss";
+import Image from "next/image";
 
 const Toggle = () => {
   const { theme, setTheme, systemTheme } = useTheme();
@@ -7,10 +10,29 @@ const Toggle = () => {
   const handleChange = () => {
     currentTheme === "dark" ? setTheme("light") : setTheme("dark");
   };
-  
+
   return (
     <div className="toggle-container">
-      <button onClick={handleChange}>{currentTheme}</button>
+      <Image
+        src={"/assets/fluent_weather-sunny-16-regular.svg"}
+        width={24}
+        height={24}
+        alt="moon"
+      />
+      <Switch.Root
+        className={styles.SwitchRoot}
+        id="switcher"
+        onCheckedChange={handleChange}
+        checked={currentTheme === "dark"}
+      >
+        <Switch.Thumb className={styles.SwitchThumb} />
+      </Switch.Root>
+      <Image
+        src={"/assets/fluent_weather-moon-16-regular.svg"}
+        width={24}
+        height={24}
+        alt="moon"
+      />
     </div>
   );
 };
