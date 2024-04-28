42be99545615ffc65af51f4b9892c449fe47af80
Eddy Rivera Echegaray <eddyriveraechegaray@Eddys-MBP.homenet.telecomitalia.it>
2024-04-28 20:29:50 +0200

    add dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import dynamic from "next/dynamic";
+import { Rubik } from 'next/font/google'
 
 const NoSSRToggle = dynamic(() => import("./components/Toggle"), { ssr: false });
 
+const rubik = Rubik({ subsets: ['latin'] })
+
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={`${styles.main} ${rubik.className}`}>
       <NoSSRToggle />
     </main>
   );
